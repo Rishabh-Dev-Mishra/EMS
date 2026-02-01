9dfd32e795f3b5ab7be8c9018cb258d131f084ea
Rishabh Dev Mishra <rishabhdevmishra388@gmail.com>
2026-02-01 22:32:57 +0530

    TaskList complete

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,15 +2,61 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div id = "tasklist" className='flex h-[55%] overflow-x-auto py-5 w-full items-center justify-start gap-5 flex-nowrap mt-10'>
-          <div className=" flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-          <div className=" flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-          <div className=" flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-          <div className=" flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-          <div className=" flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-          <div className=" flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-          <div className=" flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-          <div className=" flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+    <div id="tasklist" className='flex h-[55%] overflow-x-auto py-5 w-full items-center justify-between gap-5 flex-nowrap mt-10'>
+      <div className=" flex-shrink-0 h-full w-[300px] p-5 bg-red-400 text-white rounded-xl">
+        <div className=" flex justify-between items-center bg-red-400">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className='text-base'>20feb 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+        <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ipsam et aliquam doloribus fugit possimus.</p>
+      </div>
+
+      <div className=" flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 text-white rounded-xl">
+        <div className=" flex justify-between items-center bg-yellow-400">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className='text-base'>20feb 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+        <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ipsam et aliquam doloribus fugit possimus.</p>
+      </div>
+
+      <div className=" flex-shrink-0 h-full w-[300px] p-5 bg-green-400 text-white rounded-xl">
+        <div className=" flex justify-between items-center bg-green-400">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className='text-base'>20feb 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+        <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ipsam et aliquam doloribus fugit possimus.</p>
+      </div>
+
+      <div className=" flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 text-white rounded-xl">
+        <div className=" flex justify-between items-center bg-blue-400">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className='text-base'>20feb 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+        <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ipsam et aliquam doloribus fugit possimus.</p>
+      </div>
+
+      <div className=" flex-shrink-0 h-full w-[300px] p-5 bg-green-400 text-white rounded-xl">
+        <div className=" flex justify-between items-center bg-green-400">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className='text-base'>20feb 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+        <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ipsam et aliquam doloribus fugit possimus.</p>
+      </div>
+
+      <div className=" flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 text-white rounded-xl">
+        <div className=" flex justify-between items-center bg-blue-400">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className='text-base'>20feb 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+        <p className="text-sm mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ipsam et aliquam doloribus fugit possimus.</p>
+      </div>
+
     </div>
   )
 }
